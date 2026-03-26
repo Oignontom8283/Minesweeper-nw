deccc4bf2f78eecd71ae98aa8e3f2b18cffd50bd
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-03-26 14:11:34 +0000

    refactor: reorganize SharedState struct for clarity and consistency

--- src/common.rs
+++ src/common.rs
@@ -9,15 +9,22 @@ pub enum StateEnum {
 
 pub struct SharedState {
     pub state: StateEnum,
+    pub need_redraw: bool,
+
     pub grid: Vec<u8>,
     pub width: u8,
     pub height: u8,
     pub cursor_x: u8,
     pub cursor_y: u8,
-    pub need_redraw: bool,
+    pub first_click: bool,
+    pub num_mines: usize,
+    pub large_cells: bool,
+
+    pub asset_dirt: eadkp::Image,
 }
 
 pub enum RenderCommand {
+
     // jeu
     Cell { x: u8, y: u8 },
     Cursor { x: u8, y: u8 },
