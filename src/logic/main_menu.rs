54d425987f80d639ce6c7ef3d2f0045148b13009
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-03-26 00:24:43 +0000

    refactor: update MainMenu state to include keyboard input in update method

--- src/logic/main_menu.rs
+++ src/logic/main_menu.rs
@@ -1,6 +1,6 @@
 use crate::common::*;
 use alloc::vec::Vec;
-
+use alloc::vec;
 
 pub struct MainMenu;
 
@@ -9,12 +9,38 @@ impl StateRuntime for MainMenu {
         //
     }
 
-    fn update(_shared: &mut SharedState) -> Vec<RenderCommand> {
-        // 
+    fn update(_shared: &mut SharedState, _keyboard: eadkp::input::KeyboardState) -> Vec<RenderCommand> {
+        
+        if _shared.need_redraw {
+            _shared.need_redraw = false;
+            return vec![RenderCommand::Instruction];
+        }
+
         Vec::new()
     }
 
     fn render(_shared: &mut SharedState, _to_render: Vec<RenderCommand>) {
-        // 
+        
+        for cmd in _to_render {
+            match cmd {
+                RenderCommand::Instruction => {
+
+                    let text = "Press OK to start";
+
+                    let x =eadkp::SCREEN_RECT.width / 2 - (text.len() as u16) / 2;
+                    let y = eadkp::SCREEN_RECT.height / 2 - eadkp::LARGE_FONT.height / 2;
+
+                    eadkp::display::draw_string(
+                        text,
+                        eadkp::Point { x, y },
+                        true,
+                        eadkp::COLOR_BLACK,
+                        eadkp::COLOR_WHITE
+                    );
+                    
+                },
+                _ => {}
+            }
+        }
     }
 }
