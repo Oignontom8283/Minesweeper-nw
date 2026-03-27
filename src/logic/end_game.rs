e3f32ca2641f3f5fa7dcdb739e697b3d876017e6
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-03-27 22:36:58 +0000

    feat: implement init_end_game function and update rendering logic for end game states

--- src/logic/end_game.rs
+++ src/logic/end_game.rs
@@ -1,6 +1,11 @@
 use crate::common::*;
-use alloc::vec::Vec;
+use alloc::{vec::Vec, vec, string::ToString};
 
+pub fn init_end_game(shared: &mut SharedState, wined: bool) {
+    shared.wined = wined;
+    shared.need_redraw = true;
+    shared.state = StateEnum::EndGame
+}
 
 pub struct EndGame;
 
@@ -10,7 +15,26 @@ impl StateRuntime for EndGame {
     }
 
     fn update(_shared: &mut SharedState, _keyboard: eadkp::input::KeyboardState, _old_keyboard: eadkp::input::KeyboardState) -> Vec<RenderCommand> {
-        // 
+        
+        // Rerendre que le titre pour laisser le joueur voir ça partie
+        if _shared.need_redraw {
+            _shared.need_redraw = false;
+
+            return if _shared.wined { // Player won
+                vec![
+                    RenderCommand::TitleBackground { color: TITLE_COLOR_ENDGAME_WIN },
+                    RenderCommand::TitleText { text: TITLE_TEXT_WIN.to_string(), color: eadkp::COLOR_WHITE },
+                ]
+            } else { // Player lost
+                vec![
+                    RenderCommand::TitleBackground { color: TITLE_COLOR_ENDGAME_LOSE },
+                    RenderCommand::TitleText { text: TITLE_TEXT_LOSE.to_string(), color: eadkp::COLOR_WHITE },
+                ]
+            };
+        };
+
+        // Générer les entrées clavier
+
         Vec::new()
     }
 
