3fac821c82d31312ce061ffe04c401bbaa478395
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-04-20 20:31:27 +0000

    feat: add TextConfig struct

--- src/menu.rs
+++ src/menu.rs
@@ -11,3 +11,14 @@ pub enum VerticalAlign {
     Botton,
 }
 
+pub struct TextConfig<'a> {
+    pub text: &'a str,
+    pub pos: eadkp::Point,
+    pub h_align: HorizontalAlign,
+    pub v_align: VerticalAlign,
+    pub color: eadkp::Color,
+    pub bg_color: eadkp::Color,
+    pub font: eadkp::FontSize,
+}
+
+
