1d96032267b0f0fdf72767a3423ec75c2567d1fa
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-04-20 20:42:32 +0000

    feat: implement draw_text_configured function for rendering text with alignment

--- src/menu.rs
+++ src/menu.rs
@@ -22,3 +22,38 @@ pub struct TextConfig<'a> {
 }
 
 
+pub fn draw_text_configured(config: TextConfig) {
+
+    let font_width = config.font.width;
+    let font_height = config.font.height;
+
+    // Calculer la largeur
+    let total_width = config.text.len() as u16 * font_width;
+
+
+    // Calcule du X
+    let final_x = match config.h_align {
+        HorizontalAlign::Left => config.pos.x,
+        HorizontalAlign::Center => config.pos.x - ( total_width / 2),
+        HorizontalAlign::Right => config.pos.x - total_width,
+    };
+
+    // Calcule du Y
+    let final_y = match config.v_align {
+        VerticalAlign::Top => config.pos.y,
+        VerticalAlign::Center => config.pos.y - (font_height / 2),
+        VerticalAlign::Botton => config.pos.y - font_height,
+    };
+
+    let is_large = config.font == eadkp::LARGE_FONT;
+
+    
+    // Afficher le texte
+    eadkp::display::draw_string(
+        config.text,
+        eadkp::Point { x: final_x, y: final_y },
+        is_large,
+        config.color,
+        config.bg_color
+    )
+}
